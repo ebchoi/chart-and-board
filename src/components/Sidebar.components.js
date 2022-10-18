562c8f8ec806cca0b22237c65ecf044a2e3d7b34
ebchoi <eunbichoi1@gmail.com>
2022-10-18 12:55:53 +0900

    Add: 네브바 링크 추가

--- src/components/Sidebar.components.js
+++ src/components/Sidebar.components.js
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Sidebar = () => {
-  return <SidebarContainer>Sidebar</SidebarContainer>;
+  return (
+    <SidebarContainer>
+      <ul>
+        <li>
+          <Link to="/">사용자1</Link>
+        </li>
+        <li>
+          <Link to="/board">게시판</Link>
+        </li>
+        <li>개인설정</li>
+        <li>로그아웃</li>
+      </ul>
+    </SidebarContainer>
+  );
 };
 
-const SidebarContainer = styled.div``;
+const SidebarContainer = styled.nav``;
